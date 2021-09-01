a9ba0163f99cb7f01fef564cee11af3f3c8e2be5
SinghHarPratap <37255368+SinghHarPratap@users.noreply.github.com>
2021-09-01 19:27:15 +1000

    Assignment1_version1

---
+++ chatSystem/server/routes/createUser.js
@@ -0,0 +1,28 @@
+module.exports = function(db, app) {
+  app.post('/createUser', function(req, res) {
+    console.log("create usr API");
+    if (!req.body) {
+      return res.sendStatus(400)
+    }
+    newUser = req.body
+    username = req.body.name
+    const collection = db.collection('users')
+    //check for duplicate id's
+    collection.find({ username: username }).count((err, count) => {
+      if (count == 0) {
+        //if no duplicate
+        console.log("abc===>",req.body)
+        collection.insertOne(newUser, (err, dbres) => {
+          if (err) throw err
+          let num = dbres.insertedCount
+          //send back to client number of items instered and no error message
+          res.send({ num: num, err: null })
+        })
+      } else {
+        //On Error send back error message
+        res.send({ num: 0, err: 'duplicate item' })
+      }
+    })
+    //check for duplicate id's
+  })
+}
